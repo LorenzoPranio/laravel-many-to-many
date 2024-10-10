4d2a151e7f7de9c9e3e35a3917f9968285d6360e
LorenzoPranio <lorenzopranio02@gmail.com>
2024-10-10 16:36:19 +0200

    [UPD] Create technology seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Technology;
+
 class TechnologySeeder extends Seeder
 {
     /**
@@ -14,6 +16,13 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['HTML', 'CSS', 'Javascript', 'Vuejs', 'PHP', 'MySQL', 'Laravel', 'Symfony', 'NodeJS', 'UI/UX design'];
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Technology::generateSlug($technology);
+            
+            $new_technology->save();
+        }
     }
 }
